143b88ca3081a827c1b4a5e0a494ca4667b485e1
Ilan Ben Yosef <ilan@razorgrip.com>
2020-03-25 10:37:47 +0200

    init

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home container">
-    <h1>The Shmerlings</h1>
+    <h1>The Komi Home Page</h1>
     
     <button @click="getAll()">getAll!!!</button>
 
--- server/plugins/mongoose.js
+++ server/plugins/mongoose.js
@@ -3,7 +3,7 @@ mongoose.Promise = global.Promise;
 const password = 'admin';
 // const mongoAtlasUrl = `mongodb://ilanben:xsw23edc@ds131721.mlab.com:31721/trippo`
 
-const mongoAtlasUrl = `mongodb+srv://admin:${password}@cluster0-dvepl.azure.mongodb.net/test?retryWrites=true&w=majority`
+const mongoAtlasUrl = `mongodb+srv://admin:${password}@cluster0-bdm9r.mongodb.net/test?retryWrites=true&w=majority`
 mongoose.connect(mongoAtlasUrl);
 
 var db = mongoose.connection;
@@ -11,5 +11,4 @@ db.on('error', console.error.bind(console, 'connection error:'));
 
 db.once('open', function() {
     console.log('your mongoose is now connected!');
-    
-  });
+});
